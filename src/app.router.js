444270dc8d1f0a213692fa416600c60591791223
Eslam-Mohamed78 <eslam31043206@f-eng.tanta.edu.eg>
2024-01-29 14:02:58 +0200

    .

--- src/app.router.js
+++ src/app.router.js
@@ -8,7 +8,6 @@ import courseRouter from "./modules/course/course.router.js";
 import categRouter from "./modules/category/categ.router.js";
 import SubCategRouter from "./modules/subCategory/subCateg.router.js";
 import cartRouter from "./modules/cart/cart.router.js";
-import roomRouter from "./modules/room/room.router.js"
 import workshopRouter from "./modules/workshop/workshop.router.js"
 import { globalErrorHandler } from "./utils/asyncHandling.js";
 import cors from "cors";
