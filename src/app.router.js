a7a6a07a2d9bb3c47f13f2013de0c7668fbd2233
Eslam-Mohamed78 <eslam31043206@f-eng.tanta.edu.eg>
2024-01-29 13:12:30 +0200

    workshop upload

--- src/app.router.js
+++ src/app.router.js
@@ -3,6 +3,8 @@ import authRouter from "./modules/auth/auth.router.js";
 import userRouter from "./modules/user/user.router.js";
 import uploadRouter from "./modules/upload/upload.router.js";
 import courseRouter from "./modules/course/course.router.js"
+import workshopRouter from "./modules/workshop/workshop.router.js"
+// import roomRouter from "./modules/room/room.router.js"
 import { globalErrorHandler } from "./utils/asyncHandling.js";
 import cors from "cors";
 
@@ -16,6 +18,8 @@ const appRouter = (app, express) => {
   app.use("/user", userRouter);
   app.use("/upload", uploadRouter);
   app.use("/course", courseRouter);
+  app.use("/workshop", workshopRouter);
+  // app.use("/room", roomRouter);
 
   app.all("*", (req, res) => {
     return res.json({ message: "inVaild Path" });
